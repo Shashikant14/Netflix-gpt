e82c9b4cd64218dff637595e47bf38c5c13ec4cf
Shashikant14 <shashikant141997@gmail.com>
2024-09-27 19:51:25 +0530

    new app changes

---
+++ Netflix-gpt/README.md
@@ -0,0 +1,20 @@
+#Netflix-Gpt
+- create-react-app
+- configure tailwind.css
+
+
+#features:
+- Login/SignUp
+  - SignIn/SignUp form
+  - redirect to browse page
+-Browse (after authentication)
+    - Header
+    - Main Movie
+       - Trailer in background
+       - Title and Description
+       - MovieSuggestions
+            - MovieList * N
+            
+- Netflix-GPT
+    - Search Bar
+    - Suggestions
---
+++ Netflix-gpt/src/components/Login.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+
+export const Login = () => {
+    return(
+        <div>
+        <Header/>
+        <div>
+            <img src="https://assets.nflxext.com/ffe/siteui/vlv3/21a8ba09-4a61-44f8-8e2e-70e949c00c6f/6678e2ea-85e8-4db2-b440-c36547313109/IN-en-20240722-POP_SIGNUP_TWO_WEEKS-perspective_WEB_3457a8b1-284d-4bb5-979e-2a2e9bb342b3_large.jpg"
+            alt="background-img"
+            />
+        </div>
+        </div>
+    )
+}
+
